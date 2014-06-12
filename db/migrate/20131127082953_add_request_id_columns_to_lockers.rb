9907907f8209ce1e62aaf3d8cefe2e9312cfc262
mrevilpig <wadeliao1986@172-26-34-174.dynapool.nyu.edu>
2014-06-12 17:14:24 -0400

    DB transfor test 4

--- db/migrate/20131127082953_add_request_id_columns_to_lockers.rb
+++ db/migrate/20131127082953_add_request_id_columns_to_lockers.rb
@@ -1,7 +1,7 @@
 class AddRequestIdColumnsToLockers < ActiveRecord::Migration
   def change
     add_column :lockers, :permission_request_id, :integer
-    add_column :lockers, :access_request_id, :interger
+    add_column :lockers, :access_request_id, :integer
 
     add_index :permissions, :update_request_id
     add_index :accesses, :update_request_id
