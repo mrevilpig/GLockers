e50df3d1b4987647a44e187bc8f461c59c0fb10c
mrevilpig <wadeliao1986@172-26-34-174.dynapool.nyu.edu>
2014-06-12 17:05:31 -0400

    DB transfor test 2

--- db/migrate/20131123073758_change_trackings_type_column_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTrackingsTypeColumnType < ActiveRecord::Migration
-  def self.up
-   change_column :trackings, :type, :tinyint
-  end
-
-  def self.down
-   change_column :trackings, :type, :blob
-  end
-end
